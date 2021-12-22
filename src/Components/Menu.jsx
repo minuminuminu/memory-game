1ca4f873028523d21949c03167ff3a6ea37c2b6e
Minu <minu.kim2003@gmail.com>
2021-12-22 15:14:07 +0100

    added modal

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -8,11 +8,15 @@ import {
   UI,
 } from "../styles/MenuStyles";
 import { useState } from "react";
+import Modal from "react-modal/lib/components/Modal";
+
+Modal.setAppElement("#root");
 
 export const Menu = () => {
   const [highScore, setHighScore] = useState(
     localStorage.getItem("high_score")
   );
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
     <MenuContainer>
@@ -24,8 +28,23 @@ export const Menu = () => {
           BEST TIME: {highScore == null ? "???" : highScore}
         </HighScore>
         <PlayButton>PLAY</PlayButton>
-        <SettingsButton>SETTINGS</SettingsButton>
+        <SettingsButton
+          onClick={() => {
+            setModalIsOpen(true);
+          }}
+        >
+          SETTINGS
+        </SettingsButton>
       </UI>
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+        <button
+          onClick={() => {
+            setModalIsOpen(false);
+          }}
+        >
+          CLOSE
+        </button>
+      </Modal>
     </MenuContainer>
   );
 };
